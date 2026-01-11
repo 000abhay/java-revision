4db89a6a5c98a92fda17416cf397c3719ce72939
000abhay <abhayandblue@gmail.com>
2026-01-11 18:01:20 +0530

    sdff

---
+++ src/array/Input.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+public class Input {
+    
+
+    public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+     System.out.println("enter ");   
+     int n=sc.nextInt();
+     String str=sc.next();
+     sc.nextLine();
+     
+     String str2=sc.nextLine();
+     System.out.println("number is "+n);
+     System.out.println("string is "+str);
+        System.out.println("string is "+str2);
+
+
+    }
+}
---
+++ src/array/teat.java
@@ -0,0 +1,9 @@
+public class teat {
+    public static void main(String[] args) {
+        System.out.println("Java is working ✅");
+
+        int a = 5;
+        int b = 7;
+        System.out.println("Sum = " + (a + b));
+    }
+}
